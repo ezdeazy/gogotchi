392f6850a8d4e4384821d2f3bbf6e922eadfae73
peter <peterbradford0@gmail.com>
2022-01-12 22:42:01 -0700

    ^

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func echo(r chan string) {
+	sb := strings.Builder{}
+	for {
+		select {
+		case v, ok := <-r:
+			sb.WriteString(strings.TrimSpace(v))
+			sb.WriteString(" lastname")
+			fmt.Println(sb.String())
+			fmt.Println(ok)
+			fmt.Print("Enter your name: ")
+			sb.Reset()
+		}
+	}
+}
+
+func main() {
+	r := make(chan string, 0)
+	go echo(r)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your name: ")
+	for {
+		name, _ := reader.ReadString('\n')
+		r <- name
+	}
 }
